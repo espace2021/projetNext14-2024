07d1a7c06aa90e3470cc8129022eb18045758ab2
sh <sandrahammamitlili@gmail.com>
2023-12-31 15:50:10 +0100

    crud-commit

---
+++ src/app/(admin)/admin/dashboard/page.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const page = () => {
+  return (
+  <div className="container">
+     <img className="img-fluid"
+      src="https://res.cloudinary.com/iset-sfax/image/upload/v1704030008/images/dashboardTracking_pmhhtv.png"
+      width="1200" height="900"
+      alt="dashboard"
+       />
+       
+</div>
+  )
+}
+
+export default page
